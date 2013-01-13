f3b01328d6b6941047cb30942b5a6981238fffae
Nathan Osman <admin@quickmediasolutions.com>
2013-01-13 15:06:14 -0800

    Added code to probe function.

--- webmail_notifier.c
+++ webmail_notifier.c
@@ -17,13 +17,8 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/slab.h>
 #include <linux/usb.h>
-
-/* Define the license and provide a brief description of our module. */
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Nathan Osman <admin@quickmediasolutions.com>");
-MODULE_DESCRIPTION("Dream Cheeky Webmail Notifier driver");
-MODULE_VERSION("1.0");
 
 /* Utility macro for logging error messages. */
 #define WN_ERR(e) printk(KERN_DEBUG e);
@@ -34,7 +29,7 @@ MODULE_VERSION("1.0");
 
 /* We store some device-specific information in this struct. */
 struct usb_wn {
-    //...
+    struct usb_device * udev;
 };
 
 /* Define the table listing the vendor and device IDs of devices
@@ -83,13 +78,37 @@ static struct file_operations wn_fops = {
 static int wn_probe(struct usb_interface * interface,
                     const struct usb_device_id * id)
 {
+    struct usb_device * udev = interface_to_usbdev(interface);
+    struct usb_wn * dev = NULL;
+    
+    /* Ensure that the interface exists. */
+    if(!udev)
+    {
+        WN_ERR("interface_to_usbdev returned NULL");
+        return -ENODEV;
+    }
+    
+    /* Allocate storage space for the usb_wn struct and store
+       it with the interface. */
+    dev = kzalloc(sizeof(struct usb_wn), GFP_KERNEL);
+    if(!dev)
+    {
+        WN_ERR("unable to allocate memory for usb_wn struct");
+        return -ENOMEM;
+    }
+    
+    /* Associate the usb_wn with the interface. */
+    usb_set_intfdata(interface, dev);
     return 0;
 }
 
 /* Called when one of the devices is disconnected. */
 static void wn_disconnect(struct usb_interface * interface)
 {
-    /* Simply do nothing at this point. */
+    /* Retrieve the data for the device and free it. */
+    struct usb_wn * dev = usb_get_intfdata(interface);
+    usb_set_intfdata(interface, NULL);
+    kfree(dev);
 }
 
 static struct usb_driver wn_driver = {
@@ -119,3 +138,9 @@ static void __exit usb_wn_exit(void)
 
 module_init(usb_wn_init);
 module_exit(usb_wn_exit);
+
+/* Define the license and provide a brief description of our module. */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Nathan Osman <admin@quickmediasolutions.com>");
+MODULE_DESCRIPTION("Dream Cheeky Webmail Notifier driver");
+MODULE_VERSION("1.0");
